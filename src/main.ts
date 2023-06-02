fd84962083dd4f7eca5fa499bc711b50c82b4859
dumsterB <dumsterfordevelop@gmail.com>
2023-06-02 06:49:19 +0500

    add gitIgnore and update main path

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
+import ChattingAi from './components/widget.vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+createApp(App).component('ChattingAi', ChattingAi).mount('#app')
